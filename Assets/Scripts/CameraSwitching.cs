67161d79462139853e8d4def28eb980a2666dbcd
GuillemArman <guillemarman8@gmail.com>
2019-10-30 20:15:27 +0100

    Camera Switching

---
+++ Assets/Scripts/CameraSwitching.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitching : MonoBehaviour
+{
+
+    public Camera[] cams;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Camera1()
+    {
+        cams[0].enabled = true;
+        cams[1].enabled = false;
+
+
+    }
+
+    public void Camera2()
+    {
+        cams[0].enabled = false;
+        cams[1].enabled = true;
+
+    }
+}
