a888cfd82fafb0b9ec8f810850ee7c4a37ba9f0d
GuillemArman <guillemarman8@gmail.com>
2019-11-20 10:59:12 +0100

    Game Manager WiP

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using NodeCanvas.Framework;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    GlobalBlackboard bb;
+
+
+    public static GameManager gameManager;
+    public enum gameStates { Day, Night, GameOver };
+    public gameStates gameState = gameStates.Day;
+
+    public int score = 0; //total score = money
+    public int completedCycles = 0; //Day+Night cycles
+    public bool winCondition = false;
+    public int winCycles = 3; //number of completed cycles needed to beat the game
+
+    public GameObject Spawner;
+
+    public Text Currency;
+    public string Currency_string;
+
+    int Money = 0;
+    public int Days = 1;
+
+    bool first_time;
+
+    int tmp;
+    public bool loaded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadEverything();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (loaded != true)
+        {
+            LoadEverything();
+        }
+
+        bb.SetValue("Days", Days);
+
+        switch (gameState)
+        {
+            case gameStates.Day:
+
+                if (first_time == true)
+                {
+                    
+                   
+                    Days += 1;
+                   
+                    first_time = false;
+
+                }
+               
+
+
+                Spawner.SetActive(false);
+                break;
+            case gameStates.Night:
+
+                Spawner.SetActive(true);
+
+                first_time = true;
+
+           
+                break;
+            case gameStates.GameOver:
+                break;
+
+        }
+
+    
+
+    }
+
+    void LoadEverything()
+    {
+        if (gameManager == null)
+            gameManager = gameObject.GetComponent<GameManager>();
+
+        bb = GetComponent<GlobalBlackboard>();
+
+
+        Money = 0;
+        Days = 1;
+
+
+        bb.SetValue("Money", Money);
+        bb.SetValue("Days", 0);
+
+        Spawner = GameObject.Find("Spawner");
+
+        loaded = true;
+
+    }
+
+ }
