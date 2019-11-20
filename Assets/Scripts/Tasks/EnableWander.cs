6c39c65d50194a5d5f506d2c7aad49066243e066
Marc de Pedro <marcdp2012@gmail.com>
2019-11-20 16:29:46 +0100

    BT Cop (1/2)

---
+++ Assets/Scripts/Tasks/EnableWander.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NodeCanvas.Framework;
+
+public class EnableWander : ActionTask
+{
+    private SteeringWander wander;
+    public bool enabled;
+    
+    protected override void OnExecute()
+    {
+        wander = agent.GetComponent<SteeringWander>();
+
+        if (enabled) wander.enabled = true;
+        else wander.enabled = false;
+
+        EndAction();
+    }
+}
---
+++ Assets/Scripts/Tasks/GoTo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NodeCanvas.Framework;
+
+public class GoTo : ActionTask
+{
+    private MoveNavMesh nav_move;
+
+    private Vector3 new_target;
+    public BBParameter<Vector3> bb_target;
+
+    protected override void OnExecute()
+    {   
+        nav_move = agent.GetComponent<MoveNavMesh>();
+
+        new_target = bb_target.value;
+        nav_move.SetDestination(new_target);
+        EndAction();
+    }
+}
